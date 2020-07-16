239b45b37142ce33ca7e31fc2797cfe45c54c488
Heraldo <hprosa@globo.com>
2020-07-16 01:04:45 -0300

    Adicionar arquivos de projeto.

---
+++ Migrations/20200715183751_initial.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EFCore.WebAPI.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Batalhas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    Descricao = table.Column<string>(nullable: true),
+                    DtInício = table.Column<DateTime>(nullable: false),
+                    DtFim = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Batalhas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Herois",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    BatalhaId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Herois", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Herois_Batalhas_BatalhaId",
+                        column: x => x.BatalhaId,
+                        principalTable: "Batalhas",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Aramas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(nullable: true),
+                    HeroiId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Aramas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Aramas_Herois_HeroiId",
+                        column: x => x.HeroiId,
+                        principalTable: "Herois",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Aramas_HeroiId",
+                table: "Aramas",
+                column: "HeroiId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Herois_BatalhaId",
+                table: "Herois",
+                column: "BatalhaId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Aramas");
+
+            migrationBuilder.DropTable(
+                name: "Herois");
+
+            migrationBuilder.DropTable(
+                name: "Batalhas");
+        }
+    }
+}
---
+++ Models/Batalha.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EFCore.WebAPI.Models
+{
+    public class Batalha
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DtInício { get; set; }
+        public DateTime DtFim { get; set; }
+        public List<HeroiBatalha> HeroisBatahas { get; set; }
+    }
+}
